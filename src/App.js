27086cfefeefb093375f9e81c07ebfaa0bc0587d
Chetan Parsiya <chetanparsiya207@gmail.com>
2022-06-08 16:37:57 +0530

    React Project Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
+import {ToastContainer} from  "react-toastify"
+import 'react-toastify/dist/ReactToastify.minimal.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <ToastContainer />
+    <Router>
+      <Routes>
+        <Route  path="/" element={<Home/>}/>
+        <Route  path="/login" element={<Login/>}/>
+        <Route  path="/register" element={<Register/>}/>
+      </Routes>
+      
+        
+    </Router>
     </div>
   );
 }
